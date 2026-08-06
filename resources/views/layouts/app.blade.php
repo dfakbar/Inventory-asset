f12f183fc1338c8991ea81b4c671735873f08c76
dfakbar <dwifahyuakbar1225@gmail.com>
2026-08-06 14:39:42 +0700

    update cuy2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -220,7 +220,7 @@
 {{-- ── Sidebar ── --}}
 <nav id="sidebar">
     <a class="sidebar-brand" href="{{ route('dashboard') }}">
-        <img src="{{ asset('favicon.png') }}" alt="Logo" class="sidebar-logo">
+        <img src="{{ asset('images/kobinlogo.webp') }}" alt="Logo" class="sidebar-logo">
         Asset<span class="accent">MS</span>
     </a>
 
--- resources/views/sop_documents/pdf/_header.blade.php
+++ resources/views/sop_documents/pdf/_header.blade.php
@@ -2,9 +2,6 @@
 <div style="border-bottom: 3px solid #0d6efd; padding-bottom: 8px; margin-bottom: 14px;">
     <table style="width:100%; border-collapse: collapse;">
         <tr>
-            <td style="width:64px; vertical-align: middle;">
-                <img src="{{ public_path('favicon.png') }}" alt="Logo" style="width:52px; height:52px; object-fit:contain;">
-            </td>
             <td style="vertical-align: middle;">
                 <div style="font-size:14pt; font-weight:bold; color:#0d6efd;">{{ config('app.name', 'AssetMS') }}</div>
                 <div style="font-size:8.5pt; color:#555;">Sistem Informasi Manajemen Aset Perusahaan</div>
